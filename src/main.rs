4dd869dd816d14fee634e83e0b08302d3ea411f7
Shreyash Bubane <bubaneshreyash3@gmail.com>
2024-12-02 00:07:12 +0530

    add template for generating project

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
+    {% if esp -%}
+    // It is necessary to call this function once while running on ESP platform. 
+    // Otherwise some patches to the runtime implemented by esp-idf-sys might not link properly.
+    // See https://github.com/esp-rs/esp-idf-template/issues/71
+    {% if linux -%}
+    #[cfg(target_os = "espidf")]
+    {% endif -%}
+    esp_idf_svc::sys::link_patches();
+
+    {% endif -%}
+
     println!("Hello, world!");
 }
