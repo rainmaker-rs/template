7c5d29e5dff1b9e8d11751f9710e4c2184fde6da
Shreyash Bubane <bubaneshreyash3@gmail.com>
2024-12-03 23:08:04 +0530

    add template generation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+{% if template -%}
+mod utils;
+
+use rainmaker::{node::{self, Node}, Rainmaker};
+{% endif -%}
+
+{% if template -%}
+fn main() -> anyhow::Result<()> {
+{% else -%}
 fn main() {
+{% endif -%}
     {% if esp -%}
     // It is necessary to call this function once while running on ESP platform. 
     // Otherwise some patches to the runtime implemented by esp-idf-sys might not link properly.
@@ -10,5 +20,33 @@ fn main() {
 
     {% endif -%}
 
-    println!("Hello, world!");
+    {% if template -%}
+    {% if template_logger -%}
+    utils::initializse_logger();
+    
+    {% endif -%}
+
+    let rmaker = Rainmaker::init()?;
+
+    let mut node = Node::new(rmaker.get_node_id());
+    node.set_info(node::Info{
+        name: String::from("{{ node_name }}"),
+        fw_version: String::from("1.0"),
+    });
+
+    /* Add custom devices here */
+
+    {% if template_wifi -%}
+    utils::connect_wifi(rmaker, "{{ prov_name }}")?;
+    {% endif -%}
+    rmaker.register_node(node);
+
+    log::info!("Starting RainMaker Agent...");
+    rmaker.start()?;
+
+    Ok(())
+
+    {% else -%}
+    println!("Hello, World");
+    {%- endif %}
 }
---
+++ src/utils.rs
@@ -0,0 +1,83 @@
+{% if template -%}
+{% if template_wifi -%}
+use components::{
+    persistent_storage::NvsPartition,
+    wifi::{WifiClientConfig, WifiMgr},
+    wifi_prov::{WiFiProvMgrBle, WifiProvBleConfig},
+};
+use rainmaker::Rainmaker;
+use std::sync::{Arc, Mutex};
+
+{% endif -%}
+
+{%- if template_logger -%}
+pub fn initializse_logger() {
+    {% if linux -%}
+    {% if esp -%}
+    #[cfg(target_os = "linux")]
+    {% endif -%}
+    simple_logger::init_with_level(log::Level::Info).unwrap();
+
+    {%- endif %}
+
+    {%- if esp -%}
+    {% if linux %}
+
+    #[cfg(target_os = "espidf")]
+    {% endif -%}
+    esp_idf_svc::log::EspLogger::initialize_default();
+
+    {%- endif %}
+}
+
+{% endif -%}
+
+{%- if template_wifi -%}
+pub fn connect_wifi(
+    rmaker: &Rainmaker,
+    name: &str
+) -> anyhow::Result<()> {
+    let nvs_partition = NvsPartition::new("nvs")?;
+    wifi_arc_mutex = Arc::new(Mutex::new(WifiMgr::new()?));
+
+    let prov_config = WifiProvBleConfig {
+        service_name: format!("PROV_{}", name),
+        ..Default::default()
+    };
+    let mut prov_mgr = WiFiProvMgrBle::new(
+        wifi_arc_mutex.clone(),
+        prov_config,
+        nvs_partition,
+        components::protocomm::ProtocommSecurity::new_sec1(None),
+    )?;
+    if let Some((ssid, password)) = prov_mgr.is_provisioned() {
+        log::info!("Node already provisioned. Trying to connect");
+        let mut wifi = wifi_arc_mutex.lock().unwrap();
+        let config = WifiClientConfig {
+            ssid,
+            password,
+            ..Default::default()
+        };
+        wifi.set_client_config(config)?;
+        wifi.start()?;
+        wifi.assured_connect();
+        drop(prov_mgr);
+    } else {
+        log::info!("Node not provisioned. Starting WiFi provisioning.");
+        rmaker.reg_user_mapping_ep(&mut prov_mgr);
+        prov_mgr.start()?;
+        prov_mgr.wait_for_provisioning();
+        std::thread::spawn(move || {
+            std::thread::sleep(std::time::Duration::new(30, 0));
+            log::info!("Stopping WiFi provisioning");
+            drop(prov_mgr);
+        });
+    }
+
+    // WiFi disconnects if dropped
+    std::mem::forget(wifi_arc_mutex);
+
+    Ok(())
+}
+{% endif -%}
+{% endif -%}
